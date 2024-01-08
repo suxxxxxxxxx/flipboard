b2599aebef700dfa0dc8fbb98b911cf467acf2e6
suxxxxxxxxx <2165833853@qq.com>
2024-01-08 11:36:47 +0800

    App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
-  <div>
+    <div>
 
-  </div>
+    </div>
 </template>
 
-<script>
-export default {
+<script setup>
 
-}
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+
+</style>
